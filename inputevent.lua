c0cb34683980adfa00d344e1dfcfef54d95db67d
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2023-02-13 11:08:09 +0800

    Add command_split

--- inputevent.lua
+++ inputevent.lua
@@ -116,9 +116,41 @@ function command(command)
     return mp.command(command)
 end
 
+function command_split(command)
+    local result = {}
+    local separator = ";"
+    local quo = {'"', "'"}
+    local quo_stack = {}
+    local temp_str = ""
+
+    for i = 1, #command do
+        local char = string.sub(command,i,i)
+
+        if char == separator and #quo_stack == 0 then
+            result = table.push(result, temp_str)
+            temp_str = ""
+        elseif table.has(quo, char) then
+            temp_str = temp_str .. char
+            if quo_stack[#quo_stack] == char then
+                quo_stack = table.filter(quo_stack, function(i, v) return i ~= #quo_stack end)
+            else
+                quo_stack = table.push(quo_stack, char)
+            end
+        else
+            temp_str = temp_str .. char
+        end
+    end
+
+    if #temp_str then
+        result = table.push(result, temp_str)
+    end
+
+    return result
+end
+
 function command_invert(command)
     local invert = ""
-    local command_list = command:split(";")
+    local command_list = command_split(command)
     for i, v in ipairs(command_list) do
         local trimed = v:trim()
         local subs = trimed:split("%s*")
@@ -199,7 +231,7 @@ function InputEvent:emit(event)
     end
 
     local expand = mp.command_native({'expand-text', cmd})
-    if #cmd:split(";") == #expand:split(";") then
+    if #command_split(cmd) == #command_split(expand) then
         cmd = mp.command_native({'expand-text', cmd})
     else
         mp.msg.warn("Unsafe property-expansion: " .. cmd)
